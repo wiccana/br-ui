17b2b673f81c6ff22c4b91008609a6b691c8be55
bg-multimedia <48929583+bg-multimedia@users.noreply.github.com>
2020-10-11 22:34:12 -0300

    basic page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'primevue/resources/themes/saga-blue/theme.css'                           //icons
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+import 'primevue/resources/themes/saga-blue/theme.css'
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import Dropdown from 'primevue/dropdown'
+import 'primeflex/primeflex.css'
+
+
+Vue.component('InputText', InputText);
+Vue.component('Button', Button);
+Vue.component('Dropdown', Dropdown);
+
 Vue.config.productionTip = false
 
 new Vue({
