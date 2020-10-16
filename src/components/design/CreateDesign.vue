8118b3f74c9ad94521231ed26bd48a18799aa2da
bg-multimedia <48929583+bg-multimedia@users.noreply.github.com>
2020-10-16 01:00:42 -0300

    v-show

--- src/components/design/CreateDesign.vue
+++ src/components/design/CreateDesign.vue
@@ -1,31 +1,51 @@
 <template>
-  <NewDesignForm/>
+  <div>
+    <div  v-show="showPendingDesign">
+    <DesignSteps/>
+    </div>
+    <div v-show="!showPendingDesign">
+      <NewDesignForm/>
+    </div>
+  </div>
+  
 </template>
 
 <script>
 import NewDesignForm from './NewDesignForm.vue'
+import DesignSteps from './DesignSteps.vue'
+import axios from 'axios';
 export default {
   name: 'CreateDesign',
    components: {
-     NewDesignForm
+     NewDesignForm,
+     DesignSteps
+   },
+   created() {
+      this.checkUnfinished();
+   },
+   data () {
+      return {
+        unfinished: false 
+    }
+   },
+  computed:{
+      showPendingDesign(){
+      return this.unfinished;
+       }
+    },
+   methods: {
+     checkUnfinished(){
+        axios.get("http://localhost:8080/pdesign?finished=false", null).then(result => {
+          if(result.data.length > 0){
+            console.log("unfinished: " + result.data + " length " + result.data.length, " new Form should not be displayed");
+              this.unfinished = true;
+              this.showPendingDesign();
+            }
+              })
+              .catch(error => {
+                console.log(error);
+              });
+     }
    }
-}
+ }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/design/DesignSteps.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      Design Steps by BR
+  </div>
+  
+</template>
+
+<script>
+// import NewDesignForm from './NewDesignForm.vue'
+// import DesignSteps from './DesignSteps.vue'
+// import axios from 'axios';
+export default {
+  name: 'DesignSteps',
+//    components: {
+//      NewDesignForm,
+//      DesignSteps
+//    },
+//    created() {
+//       this.checkUnfinished();
+//    },
+//    data () {
+//       return {
+//         unfinished: false 
+//     }
+//    },
+//    methods: {
+//      checkUnfinished(){
+//         axios.get("http://localhost:8080/pdesign?finished=false", null).then(result => {
+//                 console.log("unfinished: " + result.data);
+//                 if(result.data.length > 0) this.unifinished = true;
+//               })
+//               .catch(error => {
+//                 console.log(error);
+//               });
+//      }
+//    }
+ }
+</script>
+
